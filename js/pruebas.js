8d999f1aa30f1590dd1bb5f6b544da4bfab8236a
ethanzito <Tommys.Worker@gmail.com>
2024-01-12 19:09:22 -0300

    Crear funciones de validacion

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,15 +1,52 @@
 function probarValidarNombre() {
-  console.assert(
-      validarNombre('') === 'Este campo debe tener al menos 1 caracter',
-      'Validar nombre no validó que el nombre no sea vacío',
-  );
+    console.assert(
+        validarNombre('') === 'El campo Nombre no puede estar vacio',
+        'Validar nombre no validó que el nombre no sea vacío',
+    );
+  
+    console.assert(
+        validarNombre(
+            '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
+        'El campo Nombre no debe ser mayor a 10 caracteres',
+        'Validar nombre no validó que el nombre sea menor a 50 caracteres',
+    );
+  
+    console.assert(
+        validarNombre('!@#%1^&*(') === 
+        'El campo Nombre no acepta numeros o signos',
+        'Validar nombre no validó que el nombre posea numeros o signos',
+    );
+}
 
-  console.assert(
-      validarNombre(
-          '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
-      'Este campo debe tener menos de 50 caracteres',
-      'Validar nombre no validó que el nombre sea menor a 50 caracteres',
-  );
+function probarValidarCiudad() {
+    console.assert(
+        validarCiudad('') === 'El campo Ciudad no puede estar vacio',
+        'Validar ciudad no validó que ciudad no sea vacío',
+    );
+}
+
+function probarValidarDescripcion() {
+    console.assert(
+        validarDescripcionRegalo('') === 
+        'El campo Descripcion no puede estar vacio',
+        'Validar descripcion no validó que el nombre no sea vacío',
+    );
+  
+    console.assert(
+        validarDescripcionRegalo(
+            '1111111111111111111111111111111111111111111111111111111111111') ===
+        'El campo Descripcion no debe ser mayor a 50 caracteres',
+        'Validar descripcion no validó que descripcion sea menor a 50 caracteres',
+    );
+  
+    console.assert(
+        validarDescripcionRegalo('!@#%1^&*(_{}"|:6') === 
+        'El campo Descripcion no acepta numeros o signos',
+        'Validar descripcion no validó que descripcion posea numeros o signos',
+    );
 }
 
 probarValidarNombre();
+probarValidarCiudad();
+probarValidarDescripcion();
+  
